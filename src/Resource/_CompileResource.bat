7eef15cef401d3800513f8058b103057d9a35383
Robert Beer <58856981+JARVIS-7@users.noreply.github.com>
2019-12-30 19:29:57 +0100

    Update _CompileResource.bat

--- src/Resource/_CompileResource.bat
+++ src/Resource/_CompileResource.bat
@@ -1,4 +1,5 @@
-rem Diese Datei ausführen, um auf schnelle Weise die Resource-Datei neu zu kompilieren
+rem Diese Datei ausfÃ¼hren, um auf schnelle Weise die Resource-Datei neu zu kompilieren
 rem ----------------------------------------------------------------------------------
+
 
 RC /r /fo ..\BOM.res BOM.rc
